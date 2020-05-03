577b00c6e75dddd84cdd772d7258c40ebe0923a2
leyuri <leyuri97@gmail.com>
2020-05-03 12:11:53 +0900

    Add redux-devtools to store

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # 00mask
 
 
+##### Tip
+
+redux-dev-tool을 만드려면 스토어에 해당 코드를 넣어줘야 함
+
+
+```
+ const store = createStore(
+   reducer, /* preloadedState, */
++  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+ );
+```
+
+미들웨어랑 같이 쓸 경우는?
+
+```
+  import { createStore, applyMiddleware, compose } from 'redux';
+
++ const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
++ const store = createStore(reducer, /* preloadedState, */ composeEnhancers(
+- const store = createStore(reducer, /* preloadedState, */ compose(
+    applyMiddleware(...middleware)
+  ));
+```
+
+
+
+##### error
--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux'
-import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 // redux-logger : 디버깅 할때 보려고..
 import logger from 'redux-logger' 
+import { createMuiTheme, makeStyles, ThemeProvider } from '@material-ui/core/styles';
 
 
 
@@ -13,18 +14,31 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import rootReducer from './reducers/index';
 
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 // store 만들어주기
-const store = createStore(rootReducer, applyMiddleware(logger,thunk));
+const store = createStore(
+  rootReducer, 
+  composeEnhancers(applyMiddleware(logger,thunk)));
+
+
+const theme = createMuiTheme({
+  palette: {
+    type: 'dark',
+  },
+});
 
 // react-redux 바인딩 시켜주기
 
 ReactDOM.render(
   // Provider를 넣어 이 밑의 컴포넌트들에 전부다 store가 삽입이 되는 것. 
   <Provider store={store}>
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+
+    </React.StrictMode>
   </Provider>,
   document.getElementById('root')
 );
@@ -33,3 +47,8 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+
+/*
+ThemeProvider 내부에 있는 모든 컴포넌트들에 전해주는!
+*/
